b51bd1e2e3f4f97264f5b0a0b09898241e349568
huangzesen <hzsbazinga@outlook.com>
2022-06-18 15:20:06 -0700

    Update TSUtilities.py

--- TSUtilities.py
+++ TSUtilities.py
@@ -1140,7 +1140,7 @@ def hampel_filter_forloop_numba(input_series, window_size, n_sigmas=3):
     
 
 @jit(nopython=True, parallel=True)      
-def smoothing_function(x,y, window=2, pad = 1):
+def smoothing_function_obsolete(x,y, window=2, pad = 1):
     xoutmid = np.zeros(len(x))*np.nan
     xoutmean = np.zeros(len(x))*np.nan
     yout = np.zeros(len(x))*np.nan
@@ -1155,6 +1155,55 @@ def smoothing_function(x,y, window=2, pad = 1):
                 xoutmid[i] = x[i] + np.log10(0.5) * (x[i] - x[e])
                 xoutmean[i] = np.nanmean(x[i:e])
     return xoutmid, xoutmean, yout 
+
+
+@jit(nopython=True, parallel=True)      
+def smoothing_function(x,y, window=2, pad = 1):
+    def bisection(array,value):
+        '''Given an ``array`` , and given a ``value`` , returns an index j such that ``value`` is between array[j]
+        and array[j+1]. ``array`` must be monotonic increasing. j=-1 or j=len(array) is returned
+        to indicate that ``value`` is out of range below and above respectively.'''
+        n = len(array)
+        if (value < array[0]):
+            return -1
+        elif (value > array[n-1]):
+            return n
+        jl = 0# Initialize lower
+        ju = n-1# and upper limits.
+        while (ju-jl > 1):# If we are not yet done,
+            jm=(ju+jl) >> 1# compute a midpoint with a bitshift
+            if (value >= array[jm]):
+                jl=jm# and replace either the lower limit
+            else:
+                ju=jm# or the upper limit, as appropriate.
+            # Repeat until the test condition is satisfied.
+        if (value == array[0]):# edge cases at bottom
+            return 0
+        elif (value == array[n-1]):# and top
+            return n-1
+        else:
+            return jl
+
+    len_x    = len(x)
+    max_x    = np.max(x)
+    xoutmid  = np.full(len_x, np.nan)
+    xoutmean = np.full(len_x, np.nan)
+    yout     = np.full(len_x, np.nan)
+    
+    for i in prange(len_x):
+        x0 = x[i]
+        xf = window*x0
+        
+        if xf < max_x:
+            #e = np.where(x  == x[np.abs(x - xf).argmin()])[0][0]
+            e = bisection(x,xf)
+            if e<len_x:
+                yout[i]     = np.nanmean(y[i:e])
+                xoutmid[i]  = x0 + np.log10(0.5) * (x0 - x[e])
+                xoutmean[i] = np.nanmean(x[i:e])
+
+    return xoutmid, xoutmean,  yout
+
 
 
 def curve_fit_log_wrap(x, y, x0, xf):  
