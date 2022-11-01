dafbbc4adf6ecc6d24c641e74aaf2b7a75c73aa2
huangzesen <hzsbazinga@outlook.com>
2022-11-01 16:59:30 -0700

    minor change

--- StructureFunctions.py
+++ StructureFunctions.py
@@ -9,74 +9,6 @@ import numpy as np
 # from TimeSeriesViewer import TimeSeriesViewer
 
 
-def CalculateMagDiagnosticsWrapper(sc, tstart, tend, freq_range = (-4,10), credentials = None, ndts = 100):
-
-    #load sigma_c
-
-    tsv = TimeSeriesViewer(sc = sc, start_time_0 = tstart, end_time_0 = tend, credentials = credentials)
-    tsv.InitFigure(tstart, tend, no_plot=True, resample_rate = '30s')
-
-    #load mag
-    t0 = tstart.strftime("%Y-%m-%d/%H:%M:%S")
-    t1 = tend.strftime("%Y-%m-%d/%H:%M:%S")
-    try:
-        names = pyspedas.psp.fields(trange=[t0,t1], 
-            datatype='mag_rtn', level='l2', time_clip=True
-        )
-    except:
-        names = pyspedas.psp.fields(trange=[t0,t1], 
-            datatype='mag_RTN', level='l2', time_clip=True,
-            username = credentials['psp']['fields']['username'], password = credentials['psp']['fields']['password']
-        )
-    data = get_data(names[0])
-    dfmag = pd.DataFrame(
-        index = data[0],
-        data = data[1]
-    )
-    dfmag.columns = ['Br','Bt','Bn']
-    dfmag.index = time_string.time_datetime(time=dfmag.index)
-    dfmag.index = dfmag.index.tz_localize(None)
-    dfmag = dfmag.resample('100ms').mean()
-
-    # dts in ms
-    dts = np.floor(1000*np.logspace(freq_range[0], freq_range[1], ndts, base=2))
-    dBvecs = []
-    dBvecnorms = []
-    dBmods = []
-    dBmodnorms = []
-
-    for i1 in range(len(dts)):
-        dt = pd.Timedelta("%dms" %(int(dts[i1])))
-
-        rolling_window = "%dms" %(int(dts[i1]))
-
-        dBvec, dBvecnorm, dBmod, dBmodnorm = CalculateMagDiagnostics(
-            dfmag['Br'], dfmag['Bt'], dfmag['Bn'],
-            dt, rolling_window
-        )
-        dBvecs.append(dBvec)
-        dBvecnorms.append(dBvecnorm)
-        dBmods.append(dBmod)
-        dBmodnorms.append(dBmodnorm)
-        
-    d = {
-        'sc': 0,
-        'start_time': tstart,
-        'end_time': tend,
-        'dfts': tsv.dfts,
-        'dfdis': tsv.dfdis,
-        'MagDiagnostics': {
-            'rolling_window': '10min',
-            'dts': dts,
-            'dBvecs': dBvecs,
-            'dBvecnorms': dBvecnorms,
-            'dBmods': dBmods,
-            'dBmodnorms': dBmodnorms
-        }
-    }
-
-    return d
-
 def MagStrucFunc(Br, Bt, Bn, scale_range, npts):
     """
     return the magnetic structure functions
@@ -193,3 +125,72 @@ def MovingFitOnSpectrum(x, y, window, limits, pad):
 
     return xloc, yindex
     
+
+
+# def CalculateMagDiagnosticsWrapper(sc, tstart, tend, freq_range = (-4,10), credentials = None, ndts = 100):
+
+#     #load sigma_c
+
+#     tsv = TimeSeriesViewer(sc = sc, start_time_0 = tstart, end_time_0 = tend, credentials = credentials)
+#     tsv.InitFigure(tstart, tend, no_plot=True, resample_rate = '30s')
+
+#     #load mag
+#     t0 = tstart.strftime("%Y-%m-%d/%H:%M:%S")
+#     t1 = tend.strftime("%Y-%m-%d/%H:%M:%S")
+#     try:
+#         names = pyspedas.psp.fields(trange=[t0,t1], 
+#             datatype='mag_rtn', level='l2', time_clip=True
+#         )
+#     except:
+#         names = pyspedas.psp.fields(trange=[t0,t1], 
+#             datatype='mag_RTN', level='l2', time_clip=True,
+#             username = credentials['psp']['fields']['username'], password = credentials['psp']['fields']['password']
+#         )
+#     data = get_data(names[0])
+#     dfmag = pd.DataFrame(
+#         index = data[0],
+#         data = data[1]
+#     )
+#     dfmag.columns = ['Br','Bt','Bn']
+#     dfmag.index = time_string.time_datetime(time=dfmag.index)
+#     dfmag.index = dfmag.index.tz_localize(None)
+#     dfmag = dfmag.resample('100ms').mean()
+
+#     # dts in ms
+#     dts = np.floor(1000*np.logspace(freq_range[0], freq_range[1], ndts, base=2))
+#     dBvecs = []
+#     dBvecnorms = []
+#     dBmods = []
+#     dBmodnorms = []
+
+#     for i1 in range(len(dts)):
+#         dt = pd.Timedelta("%dms" %(int(dts[i1])))
+
+#         rolling_window = "%dms" %(int(dts[i1]))
+
+#         dBvec, dBvecnorm, dBmod, dBmodnorm = CalculateMagDiagnostics(
+#             dfmag['Br'], dfmag['Bt'], dfmag['Bn'],
+#             dt, rolling_window
+#         )
+#         dBvecs.append(dBvec)
+#         dBvecnorms.append(dBvecnorm)
+#         dBmods.append(dBmod)
+#         dBmodnorms.append(dBmodnorm)
+        
+#     d = {
+#         'sc': 0,
+#         'start_time': tstart,
+#         'end_time': tend,
+#         'dfts': tsv.dfts,
+#         'dfdis': tsv.dfdis,
+#         'MagDiagnostics': {
+#             'rolling_window': '10min',
+#             'dts': dts,
+#             'dBvecs': dBvecs,
+#             'dBvecnorms': dBvecnorms,
+#             'dBmods': dBmods,
+#             'dBmodnorms': dBmodnorms
+#         }
+#     }
+
+#     return d
